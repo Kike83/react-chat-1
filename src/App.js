958640e828e10d22e23a4b31c11aacbb4de3e64f
KikeBA <enrique.berdichevsky@gmail.com>
2023-02-10 10:46:33 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import 'stream-chat-react/dist/css/v2/index.css';
+
+const chatClient = new StreamChat('kodemia19g');
+const userToken = 'ey00000';
+
+chatClient.connectUser(
+  {
+    id: 'koderKike',
+    name: 'koderKike',
+    image: 'https://getstream.io/random_png/?id=koder-kike-10',
+  },
+  userToken,
+);
+
+const channel = chatClient.channel('messaging', 'livestream', {
+  image: 'https://api.dicebear.com/5.x/fun-emoji/svg',
+  name: 'Chat for Groups',
+  members: ['Parent-9'],
+});
+
+const App = () => (
+  <Chat client={chatClient} theme='str-chat__theme-light'>
+    <Channel channel={channel}>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
 
 export default App;
